732e049fe24f735ee24de3074f39f8d6818d97ac
Rayleigh865 <Rayleigh865@gmail.com>
2022-11-17 21:58:35 +0900

    add dark light button

--- components/Headet.tsx
+++ components/Headet.tsx
@@ -6,6 +6,7 @@ import {
 } from '@chakra-ui/react';
 import NextLink from "next/link";
 import { FC } from 'react';
+import { ChangeThemeButton } from './ChangeThemeButton';
 
 export const Header: FC = () => {    
     return (
@@ -14,9 +15,12 @@ export const Header: FC = () => {
                 <Flex as="header" py="4" justifyContent="space-between" alignItems="center">
                     <NextLink href="/" passHref>
                         <Heading as='h1' fontSize="2xl" cursor="pointer">
-                            Rayleight411 Tech Blog
+                            Rayleight411 nextjs sample
                         </Heading>
                     </NextLink>
+                    <span className="group inline-flex items-center text-xl font-medium pl-2 pr-3 py-2">
+                        <ChangeThemeButton />
+                    </span>
                 </Flex>
             </Container>
         </Box>
